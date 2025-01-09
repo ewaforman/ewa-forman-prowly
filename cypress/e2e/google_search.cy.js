1b286d4d819ddbbcff9133e92c6fd713f8f57798
Ewa Forman <ewaforman@gmail.com>
2025-01-09 13:25:55 +0100

    add cypress tests for google search

---
+++ cypress/e2e/google_search.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+describe('Google Search with Date Filters', () => {
+  beforeEach(() => {
+    cy.visit('https://www.google.com');
+    cy.get('button').contains('Zaakceptuj wszystko').click({ force: true });
+  });
+
+  it('Searches for "prowly" without any date filter', () => {
+    cy.get('[name="q"]')
+      .type('prowly{enter}');
+
+    cy.url().should('include', 'search');
+    cy.get('#search').should('exist');
+    cy.get('h3').contains('prowly', { matchCase: false }).should('be.visible');
+  });
+
+  it('Searches for "prowly" filtered by results from the past 24 hours', () => {
+    cy.get('[name="q"]')
+      .type('prowly{enter}');
+
+    cy.url().should('include', 'search');
+    cy.get('div').contains('Narzędzia').click();
+    cy.get('span').contains('Kiedykolwiek').click({ force: true });
+    cy.get('span').contains('Ostatnie 24 godziny').click({ force: true });
+
+    cy.get('#search').should('exist');
+    cy.get('h3').contains('prowly', { matchCase: false }).should('be.visible');
+  });
+
+  it('Searches for "prowly" filtered by results from the past week', () => {
+    cy.get('[name="q"]')
+      .type('prowly{enter}');
+
+    cy.url().should('include', 'search');
+    cy.get('div').contains('Narzędzia').click();
+    cy.get('span').contains('Kiedykolwiek').click({ force: true });
+    cy.get('span').contains('Ostatni tydzień').click({ force: true });
+
+    cy.get('#search').should('exist');
+    cy.get('h3').contains('prowly', { matchCase: false }).should('be.visible');
+  });
+});
